7a2a5d93e907a5f2e329585cff7234d90c469a74
Sub Estacion <subestacion@paseolagaleria.com>
2019-04-29 05:10:05 -0400

    Ejemplo terminado

--- Mi_Menu/index.js
+++ Mi_Menu/index.js
@@ -1,12 +1,33 @@
+var ancho_vent = window.innerWidth; // ancho
+var alto_vent = window.innerHeight; // alto	
+
+//Actualizamos los datos
+setInterval(function() {	
+	ancho_vent = window.innerWidth;	
+}, 500);
+
+
+//Cuando pasemos el mouse por encima se baja 
+//Y cuando saquemos vuelve a subir
+$('#mas').hover(function() {
+	$('#mas ul').slideDown(700);
+}, function() {
+	$('#mas ul').slideUp(700);
+});
+
 
 $("#mas").click(function() {
+	if(ancho_vent > 940) {
+		$('#mas_todo').css('width', 100 + 'px');
+		slider();
+	
+	}else {		
+		slider();
+	}
+});
+
+
+
+function slider() {
 	$('#mas ul').slideToggle(700);
-});
-
-$("#prueba").click(function() {
-	var width = window.innerWidth; // ancho
-	var height = window.innerHeight; // alto
-
-	alert("Ancho: " + width);
-	alert("Alto: " + height);
-});
+}
